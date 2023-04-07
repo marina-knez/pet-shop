6330dc55cf27902d34b997dd19d12745b9efd1bd
Marina Knez <marina.knez08@gmail.com>
2023-04-07 06:17:25 +0200

    include new components in home page

--- src/routes/home/home.component.jsx
+++ src/routes/home/home.component.jsx
@@ -1,8 +1,17 @@
+import { Fragment } from 'react';
+import Directory from '../../components/directory/directory.component';
 import HeroSection from '../../components/hero-section/hero-section.component';
+import Widget from '../../components/widget/widget.component';
+import ReviewsSection from '../../components/reviews-section/reviews-section.component';
 
 const Home = () => {
     return (
-        <HeroSection />
+        <Fragment>
+            <HeroSection />
+            <Widget />
+            <Directory />
+            <ReviewsSection />
+        </Fragment>
     )
 }
 
