2de529c02e97b5b07fd4ba31b6b9c0dfb3ffff8a
Marina Knez <marina.knez08@gmail.com>
2023-04-14 06:34:53 +0200

    include product card into shop component; adjust styles

--- src/routes/shop/shop.component.jsx
+++ src/routes/shop/shop.component.jsx
@@ -1,8 +1,21 @@
+import { useContext } from 'react';
 
+import { ProductsContext } from '../../contexts/products.context'; 
+import ProductCard from '../../components/product-card/product-card.component';
+
+import { ProductsContainer } from './shop.styles';
 
 const Shop = () => {
+    const { products } = useContext(ProductsContext);
+
     return (
-        <h1>I am the shop page</h1>
+        <ProductsContainer>
+            {products.map((category) => (
+                category.items.map((product) => (
+                    <ProductCard key={product.id} product={product} />
+                ))
+            ))}
+        </ProductsContainer>
     )
 }
 
