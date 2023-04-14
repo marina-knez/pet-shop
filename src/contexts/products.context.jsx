996d6b74a647b9722456ead1e994b33658568499
Marina Knez <marina.knez08@gmail.com>
2023-04-14 06:32:57 +0200

    create product card context

---
+++ src/contexts/products.context.jsx
@@ -0,0 +1,17 @@
+import { createContext } from "react";
+
+import PRODUCTS from '../db/db.json';
+import { useState } from "react";
+
+export const ProductsContext = createContext({
+    products: []
+});
+
+export const ProductsProvider = ({children}) => {
+    const [products, setProducts] = useState(PRODUCTS);
+    const value = {products};
+
+    return(
+        <ProductsContext.Provider value={value}> {children} </ProductsContext.Provider>
+    )
+}
