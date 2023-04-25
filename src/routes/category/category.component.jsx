0f1f8933497cceb4654093151be0d30596663bff
Marina Knez <marina.knez08@gmail.com>
2023-04-25 15:05:09 +0200

    modify category component to now display only chosen category-related products; add styles

--- src/routes/category/category.component.jsx
+++ src/routes/category/category.component.jsx
@@ -1,9 +1,29 @@
+import { useContext, useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
 
+import { CategoriesContext } from "../../contexts/categories.context";
+import ProductCard from "../../components/product-card/product-card.component";
+
+import { CategoryContainer } from "./category.styles";
 
 const Category = () => {
-    return (
-        <h1>I am the category page</h1>
-    )
-}
+  const { category } = useParams();
+  const { categories } = useContext(CategoriesContext);
+  const [products, setProducts] = useState([]);
+  
+  useEffect(() => {
+    const categoryData = categories.filter((cat) => cat.categoryName === category)[0];
+    setProducts(categoryData ? categoryData.items : [])
+  }, [category, categories])
+
+  return (
+    <CategoryContainer>
+      <h2>{category}</h2>
+      {products.map((product) => (
+        <ProductCard key={product.id} product={product} />
+      ))}
+    </CategoryContainer>
+  );
+};
 
 export default Category;
