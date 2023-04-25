0e1a814e8e2ef3f06aecc809238bb881748776b0
Marina Knez <marina.knez08@gmail.com>
2023-04-25 15:01:20 +0200

    create categories preview page so as to display all categories and first four products as a preview; add styles

--- src/routes/categories-preview/categories-preview.component.jsx
+++ src/routes/categories-preview/categories-preview.component.jsx
@@ -1,8 +1,23 @@
+import { Fragment, useContext } from 'react';
 
+import { CategoriesContext } from '../../contexts/categories.context'; 
+import CategoryPreview from '../../components/category-preview/category-preview.component';
 
 const CategoriesPreview = () => {
+    const { categories } = useContext(CategoriesContext);
+
     return (
-        <h1>I am the categories preview page</h1>
+        <Fragment>
+            {
+                categories.map(category => {
+                    const products = category.items;
+                    return (
+                        <CategoryPreview key={category.categoryName} title={category.categoryName} products={products} />
+                    )
+                })
+            }
+        </Fragment>
+
     )
 }
 
