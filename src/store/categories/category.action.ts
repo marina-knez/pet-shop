7fdf0857aae26a32a94e855fb1f8c3a06e18a8db
Marina Knez <marina.knez08@gmail.com>
2023-05-25 05:32:23 +0200

    migrate category to typescript

---
+++ src/store/categories/category.action.ts
@@ -0,0 +1,26 @@
+import { createAction, Action, ActionWithPayload, withMatcher } from "../../utilities/reducer/reducer.utilities";
+import { CATEGORIES_ACTION_TYPES, Category } from "./category.types";
+import { getCategories } from '../../utilities/fetch-db/fetch-db.utilities';
+
+export type FetchCategoriesStart = Action<CATEGORIES_ACTION_TYPES.FETCH_CATEGORIES_START>;
+
+export type FetchCategoriesSuccess = ActionWithPayload<CATEGORIES_ACTION_TYPES.FETCH_CATEGORIES_SUCCESS, Category[]>;
+
+export type FetchCategoriesFailed = ActionWithPayload<CATEGORIES_ACTION_TYPES.FETCH_CATEGORIES_FAILED, Error>;
+
+export const fetchCategoriesStart = withMatcher((): FetchCategoriesStart => createAction(CATEGORIES_ACTION_TYPES.FETCH_CATEGORIES_START));
+
+export const fetchCategoriesSuccess = withMatcher((categories: Category[]): FetchCategoriesSuccess => createAction(CATEGORIES_ACTION_TYPES.FETCH_CATEGORIES_SUCCESS, categories));
+
+export const fetchCategoriesFailed = withMatcher((error: Error): FetchCategoriesFailed => createAction(CATEGORIES_ACTION_TYPES.FETCH_CATEGORIES_FAILED, error));
+
+export const fetchCategoriesAsync = () => async (dispatch) => {
+    dispatch(fetchCategoriesStart());
+
+    try {
+        const categoriesData = await getCategories();
+        dispatch(fetchCategoriesSuccess(categoriesData));
+    } catch(error) {
+        dispatch(fetchCategoriesFailed(error));
+    }
+};
