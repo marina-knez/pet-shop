d701b2f1a1b19fb3e2fd75dde816cefbc42deb2f
Marina Knez <marina.knez08@gmail.com>
2023-06-06 06:09:40 +0200

    add puppy guide content and styles

--- src/routes/guide/guide.component.tsx
+++ src/routes/guide/guide.component.tsx
@@ -1,8 +1,126 @@
-
+import { GuidePageContainer, GuidePageIntro, GuidePageTitle, GuidePageDescription, MainContentContainer, MainContentSection, MainContentSectionTitle, MainContentSectionImage, MainContentSectionDescription } from "./guide.styles";
 
 const Guide = () => {
     return (
-        <h1>I am the guide page</h1>
+        <GuidePageContainer>
+            <GuidePageIntro>
+                <GuidePageTitle>Welcome to the world of puppies!</GuidePageTitle>
+                <GuidePageDescription>We know that your little bundle of joy is irresistible, but at the same time, all the responsibilities that come with it may seem daunting. There's a lot to think about and do to ensure your best friend is happy and healthy. We're here to provide you with advice and make the first days of living with a puppy easier for you. We wish you many wonderful moments together.</GuidePageDescription>
+            </GuidePageIntro>
+            <MainContentContainer>
+                <MainContentSection>
+                    <div>
+                        <MainContentSectionTitle>Your Home</MainContentSectionTitle>
+                        <MainContentSectionDescription>
+                            <p>The arrival of a little puppy in your home can be intimidating at first. An unfamiliar space, new scents, new people – it can all be overwhelming. It's important for both you and your household members to try to restrain yourselves from being overly excited about the new puppy, at least for the first few days. Loud noises and commotion can scare them. Give them time to gradually adjust to the new situation, get to know you, and then you can celebrate together. Similarly, it's important to provide them with a designated spot in your home where they can retreat and rest without being disturbed. This will be a place they can go to if they feel scared.</p>
+                            <p>Choose a bed or a crate that will be their own secure space.</p>
+                        </MainContentSectionDescription>
+                    </div>
+                    <MainContentSectionImage src="/assets/puppy-guide/puppy-at-home.jpg" alt="Puppy at home" title="Puppy at home"/>
+                </MainContentSection>
+                <MainContentSection>
+                    <MainContentSectionImage src="/assets/puppy-guide/anxious-puppy.jpg" alt="Anxious puppy" title="Anxious puppy"/>
+                    <div>
+                        <MainContentSectionTitle>Anxious Puppy</MainContentSectionTitle>
+                        <MainContentSectionDescription>
+                            <p>Some puppies can become extremely anxious when they arrive in a new home. The unfamiliar environment causes them stress, which can be difficult for them to cope with. You can help them by using calming products based on pheromones. These products will reduce stress, calm the puppy, and facilitate their adjustment to the new home. With the help of pheromones, they will understand that they are safe in their new home and have nothing to fear.</p>
+                            <p>Some puppies may be happy and content when you're around but become anxious when you leave. Pheromones can also be helpful in this situation. Leave plenty of different toys for your pet along with the pheromones to keep them entertained, and you can also leave an item of your clothing that carries your scent to provide them with a sense of security.</p>
+                        </MainContentSectionDescription>
+                    </div>
+                </MainContentSection>
+                <MainContentSection>
+                    <div>
+                        <MainContentSectionTitle>Crate Training</MainContentSectionTitle>
+                        <MainContentSectionDescription>
+                            <p>A crate is an excellent way for your puppy to feel safe and protected. It serves as their den, a place for rest and peace. To introduce your puppy to the crate, you should put a soft blanket and plenty of fun toys inside. You can also provide their meals in the crate, to associate it with positive experiences. The crate can also aid in potty training, as dogs instinctively avoid soiling their sleeping area.</p>
+                            <p>Having a crate also gives you peace of mind, knowing that your playful little bundle won't get into mischief when you're not home. Always remember to leave enough fresh water for your pet and avoid leaving them in the crate for more than a few hours.</p>
+                        </MainContentSectionDescription>
+                    </div>
+                    <MainContentSectionImage src="/assets/puppy-guide/crate-training.jpg" alt="Crate training" title="Crate training" />
+                </MainContentSection>
+                <MainContentSection>
+                    <MainContentSectionImage src="/assets/puppy-guide/feeding.jpg" alt="Feeding" title="Feeding" />
+                    <div>
+                        <MainContentSectionTitle>Feeding</MainContentSectionTitle>
+                        <MainContentSectionDescription>
+                            <p>Puppies grow rapidly and require a lot of food. To ensure they grow into healthy dogs, it's important that the food they eat is of high quality and complete. It should contain all the necessary nutrients they need for growth and development. When selecting food for your new pet, make sure to choose one specifically formulated for puppies and with a high percentage of meat and other quality ingredients. Avoid foods that contain artificial preservatives, flavors, and similar additives. Remember that your puppy is a carnivore, and a proper diet will ensure a healthy life for them.</p>
+                            <p>When introducing new food to your pet's menu, make sure to do it gradually over a period of 5 to 7 days. This allows their digestive system enough time to adapt and helps prevent digestive issues.</p>
+                        </MainContentSectionDescription>
+                    </div>
+                </MainContentSection>
+                <MainContentSection>
+                    <div>
+                        <MainContentSectionTitle>Potty Training</MainContentSectionTitle>
+                        <MainContentSectionDescription>
+                            <p>If you want your little best friend to do their business in a specific spot, you need to teach them. Puppies can't hold it for long and often need to go potty.</p>
+                            <p>After sleeping, eating, or playing, they will likely feel the urge, so it's a great opportunity for training. Take the puppy outside to the grass or to the designated potty pad you've set up in your home and wait for them to do their business. When they succeed, enthusiastically praise them and reward them with a tasty treat. This way, they will quickly learn where to go potty. If they happen to have an accident and go in a different place, don't get upset. Puppies don't do it on purpose; they simply can't hold it for long.</p>
+                            <p>Arm yourself with patience and treats! With positive training and consistent practice, success is guaranteed for both of you.</p>
+                        </MainContentSectionDescription>
+                    </div>
+                    <MainContentSectionImage src="/assets/puppy-guide/potty-training.jpg" alt="Potty Training" title="Potty Training" />
+                </MainContentSection>
+                <MainContentSection>
+                    <MainContentSectionImage src="/assets/puppy-guide/toys.jpg" alt="Toys" title="Toys" />
+                    <div>
+                        <MainContentSectionTitle>Toys</MainContentSectionTitle>
+                        <MainContentSectionDescription>
+                            <p>Playtime is an essential part of your new best friend's day. You can never have too many toys! The more toys they have, the happier your puppy will be. Through play, they will tire themselves out, burn excess energy, and stay mentally stimulated. With enough toys, they won't feel the need to play with your belongings, reducing the chances of accidents.</p>
+                            <p>Plush, fabric, rubber, squeaky, silent, dental, balls, ropes... There are so many toys to choose from, and each one will help your little dog be happy, playful, and content.</p>
+                        </MainContentSectionDescription>
+                    </div>
+                </MainContentSection>
+                <MainContentSection>
+                    <div>
+                        <MainContentSectionTitle>Health</MainContentSectionTitle>
+                        <MainContentSectionDescription>
+                            <p>It is extremely important to protect your puppy from diseases and parasites lurking in the environment. The first protection your pet needs is vaccination against infectious diseases. In addition to infectious diseases, it is crucial to vaccinate them against rabies as well. Your veterinarian will inform you about the right time for vaccination. If you haven't done so already, be sure to visit the vet to get all the necessary information about vaccinations.</p>
+                            <p>External and internal parasites can be very unpleasant and can also cause serious illnesses. To protect your pet, make sure to provide them with a spray, spot-on treatment, or collar for external parasites, as well as a tablet for internal parasites. If you're unsure which product to choose, your veterinarian or the knowledgeable staff at ZOOCITY can help you. Prevention is key, so don't forget to protect your little furry friend.</p>
+                        </MainContentSectionDescription>
+                    </div>
+                    <MainContentSectionImage src="/assets/puppy-guide/health.jpg" alt="Health" title="Health" />
+                </MainContentSection>
+                <MainContentSection>
+                    <MainContentSectionImage src="/assets/puppy-guide/grooming.jpg" alt="Grooming" title="Grooming" />
+                    <div>
+                        <MainContentSectionTitle>Grooming</MainContentSectionTitle>
+                        <MainContentSectionDescription>
+                            <p>Regular brushing is an important part of every small dog's life. Besides being beneficial, it can also be a pleasant experience if taught properly. To start, show your puppy that the presence of the brush brings lots of tasty treats. Then, gradually begin brushing, making sure that your best friend feels comfortable throughout the process. With plenty of treats and gentle petting along the way, brushing will become their favorite ritual! Regular brushing is essential to maintain your pet's coat shiny, soft, and free from knots. It is especially useful during shedding seasons when brushing can help reduce the amount of loose hair around your home.</p>
+                            <p>In addition to brushing, you will occasionally need to bathe your puppy. Make sure to choose a shampoo specifically formulated for puppies, safe for their skin and coat. Besides brushing and bathing, you will also need to familiarize your little furry ball with nail trimming. Make the learning process gradual and reward-based, using lots of treats. It's important to make touching their paws a positive experience, and nail clippers can become another object associated with plenty of tasty rewards. Positive training works wonders here too!</p>
+                        </MainContentSectionDescription>
+                    </div>
+                </MainContentSection>
+                <MainContentSection>
+                    <div>
+                        <MainContentSectionTitle>Training</MainContentSectionTitle>
+                        <MainContentSectionDescription>
+                            <p>You can start training and teaching new behaviors to your puppy from the first day they come into your home. It's never too early to begin training! All you need is some tasty treats and patience. Practice the behaviors you want to teach your puppy at home first. Since home is familiar and there are no distractions, learning will be easier and faster. Once your puppy has mastered the behavior at home, make sure to practice it during walks as well. This will ensure that your little friend knows exactly what you want them to do at any given moment. With tasty treats, they will happily perform every behavior, even in the face of distractions. </p>
+                            <p>Positive training is the one that brings the best and long-lasting results. Your anger or frustration will only hinder the learning process. Tools like a clicker can also assist in making the learning process faster and more enjoyable. If you're unsure how to teach your best friend the desired behaviors, consider enrolling in a puppy training class where you can learn fun and useful tricks together.</p>
+                        </MainContentSectionDescription>
+                    </div>
+                    <MainContentSectionImage src="/assets/puppy-guide/training.jpg" alt="Training" title="Training" />
+                </MainContentSection>
+                <MainContentSection>
+                    <MainContentSectionImage src="/assets/puppy-guide/spaying-neutering.jpg" alt="Neutering / Spaying" title="Neutering / Spaying" />
+                    <div>
+                        <MainContentSectionTitle>Neutering / Spaying</MainContentSectionTitle>
+                        <MainContentSectionDescription>
+                            <p>Unless your puppy is a future show champion or a breeding dog obtained from a registered and responsible breeder, neutering / spaying is something you should consider. There are unfortunately too many stray dogs, and neutering / spaying helps reduce their numbers. When your puppy becomes an adult dog, typically between 12 to 24 months of age (depending on their size), they will be ready for neutering / spaying.</p>
+                            <p>By neutering / spaying your dog, you will prevent their reproduction and potential diseases that can arise when an intact dog is not bred. Additionally, you will reduce frustration caused by the production of sex hormones and prevent possible behavioral problems. Neutering  /spaying brings positive effects to your pet's life, helping them stay happier and healthier for longer.</p>
+                        </MainContentSectionDescription>
+                    </div>
+                </MainContentSection>
+                <MainContentSection>
+                    <div>
+                        <MainContentSectionTitle>Walks</MainContentSectionTitle>
+                        <MainContentSectionDescription>
+                            <p>Walks are an integral part of every dog's life. Just like everything else, your puppy needs to learn how to walk on a leash. The first thing you should teach your little friend is that a collar and leash are not scary. Put them on while you're at home, even if you're not going for a walk, so they can get used to the feeling. Reward them generously when they wear the collar. Prepare additional rewards for the moment you attach the leash to the collar, and watch their confidence grow. Now you're ready for a walk outside! </p>
+                            <p>Before going for a walk, make sure your puppy is protected with vaccinations to prevent illness. During the walk, you will encounter other dogs and people who might seem intimidating at first, but with plenty of treats and your support, your puppy will gain courage and enjoy the walks. It's important to adapt the walks to your puppy's needs and avoid putting them in uncomfortable situations. Positive experiences are crucial for your little bundle of joy to grow into a confident, happy dog. Also, be mindful not to overdo physical activity. Excessive exercise can have a negative impact on growing joints and bones.</p>
+                        </MainContentSectionDescription>
+                    </div>
+                    <MainContentSectionImage src="/assets/puppy-guide/puppy-on-a-walk.jpg" alt="Walks" title="Walks" />
+                </MainContentSection>
+            </MainContentContainer>
+        </GuidePageContainer>
     )
 }
 
---
+++ src/routes/guide/guide.styles.ts
@@ -0,0 +1,46 @@
+import styled from 'styled-components';
+
+export const GuidePageContainer = styled.div`
+    margin: 50px;
+`
+
+export const GuidePageIntro = styled.div`
+    padding-bottom: 50px;
+`
+
+export const GuidePageTitle = styled.h2`
+    font-size: 3rem;
+    text-align: center;
+`
+
+export const GuidePageDescription = styled.p`
+    font-size: 1.8rem;
+    text-align: center;
+`
+
+export const MainContentContainer = styled.div`
+    display: flex;
+    flex-direction: column;
+    gap: 5rem;
+    margin-top: 50px;
+`
+
+export const MainContentSection = styled.div`
+    display: flex;
+    flex-direction: row;
+    flex: 1;
+    grid-gap: 3rem;
+    flex-basis: 100%;
+`
+
+export const MainContentSectionTitle = styled.h3`
+    font-size: 1.8rem;
+`
+
+export const MainContentSectionImage = styled.img`
+    width: 50%;
+`
+
+export const MainContentSectionDescription = styled.div`
+    font-size: 1.2rem;
+`
