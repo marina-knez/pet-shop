2bc2ce9918b26cab11ce6eef537d5c15286769c3
Marina Knez <marina.knez08@gmail.com>
2023-06-30 05:51:55 +0200

    adjust category styles for mobile view

--- src/components/category-preview/category-preview.styles.ts
+++ src/components/category-preview/category-preview.styles.ts
@@ -26,21 +26,3 @@ export const Preview = styled.div`
         grid-template-rows: repeat(4, 1fr);
     }
 `
-
-/*export const CategoryPreviewContainer = styled.div`
-    display: flex;
-    flex-direction: column;
-    margin-bottom: 30px;
-`
-
-export const CategoryPreviewTitle = styled.span`
-    font-size: 28px;
-    margin-bottom: 25px;
-    cursor: pointer;
-`
-
-export const Preview = styled.div`
-    display: grid;
-    grid-template-columns: repeat(4, 1fr);
-    column-gap: 20px;
-`*/
--- src/routes/category/category.styles.ts
+++ src/routes/category/category.styles.ts
@@ -6,6 +6,11 @@ export const CategoryContainer = styled.div`
     column-gap: 20px;
     row-gap: 50px;
     margin: 50px;
+
+    @media screen and (max-width: 768px) {
+        grid-template-columns: repeat(1, 100%);
+        grid-template-rows: repeat(4, 1fr);
+    }
 `
 
 export const CategoryTitle = styled.h2`
