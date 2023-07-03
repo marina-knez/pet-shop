4a6f7eadf91b878790f7c981bd4ddc67816a4f63
Marina Knez <marina.knez08@gmail.com>
2023-07-03 06:26:27 +0200

    adjust styles for tablet view

--- src/routes/about/about.styles.ts
+++ src/routes/about/about.styles.ts
@@ -3,7 +3,7 @@ import styled from 'styled-components';
 export const AboutPageContainer = styled.div`
     margin: 50px;
 
-    @media screen and (max-width: 768px) {
+    @media screen and (max-width: 1024px) {
         margin: 20px;
     }
 `
@@ -14,4 +14,8 @@ export const AboutPageTitle = styled.h2`
 
 export const AboutPageContent = styled.p`
     font-size: 1.2rem;
+
+    @media screen and (max-width: 1024px) {
+        font-size: 1.4rem;
+    }
 `
