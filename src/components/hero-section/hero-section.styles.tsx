3592c7f55cea0a2bd989cc35891e19cdfe8c651c
Marina Knez <marina.knez08@gmail.com>
2023-07-07 05:30:18 +0200

    adjust slider height

--- src/components/hero-section/hero-section.styles.tsx
+++ src/components/hero-section/hero-section.styles.tsx
@@ -2,7 +2,7 @@ import styled from "styled-components";
 import { SliderContainer } from "../slider/slider.styles";
 
 export const HeroSliderContainer = styled(SliderContainer)`
-    height: 95vh;
+    height: 90vh;
 
     @media screen and (max-width: 1024px) {
         height: 90vh;
