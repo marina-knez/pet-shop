a215e477c55b890b3a643ba46be62ed318257208
Marina Knez <marina.knez08@gmail.com>
2023-08-09 06:57:45 +0200

    remove unnecessary files and dependencies

--- src/utilities/fetch-db/fetch-db.utilities.ts
+++
@@ -1,8 +0,0 @@
-import { Category } from "../../store/categories/category.types";
-
-export const getCategories = async (): Promise<Category[]> => {
-    const categoriesArray = await fetch('http://localhost:4000/dogs')
-    .then(res => res.json());
-
-    return categoriesArray as Category[];
-}
